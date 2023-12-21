ad98655b52274fe6a2e5b24fdb84bee1c046e1a8
tomjfrog <tomj@jfrog.com>
2023-12-21 13:13:53 -0600

    Added project scaffolding

---
+++ src/main/java/com/tomjfrog/oidcdemo/OidcDemoApplication.java
@@ -0,0 +1,13 @@
+package com.tomjfrog.oidcdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OidcDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OidcDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomjfrog/oidcdemo/OidcDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomjfrog.oidcdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OidcDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
