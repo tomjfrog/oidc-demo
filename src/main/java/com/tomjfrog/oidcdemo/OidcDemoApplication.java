461aefa53b87d4fbb960ea9d2ebacea78ad857c6
tomjfrog <tomj@jfrog.com>
2023-12-28 09:01:48 -0600

    Condensed application code

--- src/main/java/com/tomjfrog/oidcdemo/OidcDemoApplication.java
+++ src/main/java/com/tomjfrog/oidcdemo/OidcDemoApplication.java
@@ -1,13 +1,8 @@
 package com.tomjfrog.oidcdemo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class OidcDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OidcDemoApplication.class, args);
+		System.out.println("Hello, World!");
 	}
-
 }
